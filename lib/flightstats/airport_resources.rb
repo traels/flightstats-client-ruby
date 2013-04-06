d55d8ef80c41ab0e925147e5f1566991147f7737
diditclear <didyourupgradeclear@gmail.com>
2013-04-06 14:35:55 -0700

    airport_resources, flight_durations: add missing attributes
    Signed-off-by: diditclear <didyourupgradeclear@gmail.com>

--- lib/flightstats/airport_resources.rb
+++ lib/flightstats/airport_resources.rb
@@ -1,6 +1,7 @@
 module FlightStats
   class AirportResources < Resource
     attr_accessor :departure_gate,
+                  :departure_terminal,
                   :arrival_terminal,
                   :arrival_gate
   end
--- lib/flightstats/flight_durations.rb
+++ lib/flightstats/flight_durations.rb
@@ -3,7 +3,10 @@ module FlightStats
     attr_accessor :scheduled_block_minutes,
                   :scheduled_air_minutes,
                   :scheduled_taxi_out_minutes,
+                  :scheduled_taxi_in_minutes,
+                  :block_minutes,
+                  :air_minutes,
                   :taxi_out_minutes,
-                  :scheduled_taxi_in_minutes
+                  :taxi_in_minutes
   end
 end
