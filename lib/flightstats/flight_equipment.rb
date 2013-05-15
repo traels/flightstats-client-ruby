b27e5a07cd738ae2dfa39d5ecfdf7337f8ad6ca8
diditclear <didyourupgradeclear@gmail.com>
2013-05-15 08:34:27 -0700

    FlightEquipment: add tail_number attribute
    Signed-off-by: diditclear <didyourupgradeclear@gmail.com>

--- lib/flightstats/flight_equipment.rb
+++ lib/flightstats/flight_equipment.rb
@@ -1,6 +1,7 @@
 module FlightStats
   class FlightEquipment < Resource
     attr_accessor :scheduled_equipment_iata_code,
-                  :actual_equipment_iata_code
+                  :actual_equipment_iata_code,
+                  :tail_number
   end
 end
