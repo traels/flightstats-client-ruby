a260b811e70ae8eccf5f61186ff7817595a012a3
Benoit Liabastre <b.liabastre@gmail.com>
2014-02-20 12:53:37 +0100

    added sheduled flight

--- lib/flightstats/scheduled_flight.rb
+++ lib/flightstats/scheduled_flight.rb
@@ -4,7 +4,7 @@ module FlightStats
     @@base_path = "/flex/schedules/rest/v1/json"
 
     class << self
-      def by_flight_id(departure_code, arrival_code, year, month, day, params = {}, options = {})
+      def by_route_departing_on(departure_code, arrival_code, year, month, day, params = {}, options = {})
         from_response API.get("#{base_path}/from/#{departure_code}/to/#{arrival_code}/departing/#{year}/#{month}/#{day}", params, options)
       end
     end
