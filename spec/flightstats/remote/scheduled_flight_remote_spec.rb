4a1a92dc6fdc7d25fb8d673712e55b2f4edc8673
Benoit Liabastre <b.liabastre@gmail.com>
2014-03-26 14:16:44 +0100

    test

--- spec/flightstats/remote/scheduled_flight_remote_spec.rb
+++ spec/flightstats/remote/scheduled_flight_remote_spec.rb
@@ -3,10 +3,19 @@ require 'time'
 
 describe FlightStats::ScheduledFlight do
 
-  it 'must return a CDG departure schedule' do
-    # now + three days in seconds
-    in_three_days = Time.now + (60 * 60 * 24 * 3)
-    scheduled_flights = FlightStats::ScheduledFlight.by_route_departing_on 'CDG', 'GVA', in_three_days.year, in_three_days.month, in_three_days.day
-    scheduled_flights.first.departure_airport_fs_code.should == 'CDG'
+  describe '#by_route_departing_on' do
+    it 'must return a CDG departure schedule' do
+      in_three_days = Time.now + (60 * 60 * 24 * 3)
+      scheduled_flights = FlightStats::ScheduledFlight.by_route_departing_on 'CDG', 'GVA', in_three_days.year, in_three_days.month, in_three_days.day
+      scheduled_flights.first.departure_airport_fs_code.should == 'CDG'
+    end
+  end
+
+  describe '#by_carrier_and_flight_number_departing_on' do
+    it 'must return a CDG departure schedule' do
+      in_three_days = Time.now + (60 * 60 * 24 * 3)
+      scheduled_flights = FlightStats::ScheduledFlight.by_carrier_and_flight_number_departing_on 'AF', '111', in_three_days.year, in_three_days.month, in_three_days.day
+      scheduled_flights.first.carrier_fs_code.should == 'AF'
+    end
   end
 end
