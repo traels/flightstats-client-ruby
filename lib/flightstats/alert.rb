21a26c9bf71d952ea127b8f86740cefbd7afe20e
Artur Pañach <arturictus@gmail.com>
2015-02-13 17:49:56 +0100

    remove alerts by id

--- lib/flightstats/alert.rb
+++ lib/flightstats/alert.rb
@@ -2,12 +2,18 @@ require 'json'
 module FlightStats
   class Alert < OpenStruct
 
-    @@base_path = "/flex/alerts/rest/v1/json/create"
+    @@base_path = "/flex/alerts/rest/v1/json"
 
     class << self
       def by_arrival(carrier, flight_number, arrival_airport, year, month, day, options = {})
-        from_response API.get("#{base_path}/#{carrier}/#{flight_number}/to/#{arrival_airport}/arriving/#{year}/#{month}/#{day}",
+        from_response API.get("#{base_path}/create/#{carrier}/#{flight_number}/to/#{arrival_airport}/arriving/#{year}/#{month}/#{day}",
                               required_params,
+                              options), 'alert'
+      end
+
+      def delete_by_id(id, options = {})
+        from_response API.get("#{base_path}/delete/#{id}",
+                              {},
                               options), 'alert'
       end
 
