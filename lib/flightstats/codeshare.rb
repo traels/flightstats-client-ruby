58a501678553e026330dd8cd0e0b43563b6482e2
Alex Kremer <alex.kremer@nor1.com>
2016-05-03 21:01:20 -0600

    Add carrier information to codeshare class

--- lib/flightstats/codeshare.rb
+++ lib/flightstats/codeshare.rb
@@ -2,6 +2,7 @@ module FlightStats
   class Codeshare < Resource
     attr_accessor :fs_code,
                   :flight_number,
+                  :carrier,
                   :relationship
   end
 end
